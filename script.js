d5437625c47b696342b347232e652450106eda97
cidadcruz cida <gabriellacsilva2002@gmail.com>
2023-02-05 22:10:27 -0300

    pizzas aparecendo

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ pizzaJson.map((item,index) => {
     //pega a classe pizza-item pelo DOM e clona
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+    //coloca as informações nos lugares
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+
     //adiciona as pizzas na classe pizza-area
     c('.pizza-area').append(pizzaItem);
 
