a785e0ecc00a2915fff9b866da6aae397a0cbbf5
cidadcruz cida <gabriellacsilva2002@gmail.com>
2023-02-05 21:29:11 -0300

    querrySelectors ok

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log(pizzaJson);
+
+//função para retornar o querrySelector
+const c = ( e ) => {
+    return document.querySelector(e);
+}
+
+const cAll = ( e ) => {
+    return document.querySelectorAll(e);
+}
+
+pizzaJson.map((item,index) => {
+    // console.log(item)
+
+    //pega a classe pizza-item pelo DOM e clona
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+    //adiciona as pizzas na classe pizza-area
+    c('.pizza-area').append(pizzaItem);
+
+});
