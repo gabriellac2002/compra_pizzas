ba03cfbb64291bf790d94640e072a8df9ce04271
cidadcruz cida <gabriellacsilva2002@gmail.com>
2023-02-10 01:56:43 -0300

    partiu carrinho

--- script.js
+++ script.js
@@ -82,3 +82,23 @@ function closeModal () {
 cAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
     item.addEventListener('click', closeModal);
 });
+
+//botões mais e menos
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if(modalQt > 1 ){
+        modalQt --;
+        c('.pizzaInfo--qt').innerHTML = modalQt ;
+    }
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQt ++;
+    c('.pizzaInfo--qt').innerHTML = modalQt ;
+});
+
+cAll('.pizzaInfo--size').forEach((size,sizeIndex) => {
+    size.addEventListener('click', (e) => {
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
