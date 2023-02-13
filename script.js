84aab420e0658a1258f1bf3d3a09fb62f00245e5
cidadcruz cida <gabriellacsilva2002@gmail.com>
2023-02-13 20:19:52 -0300

    updateCart()

--- script.js
+++ script.js
@@ -142,6 +142,21 @@ c('.pizzaInfo--addButton').addEventListener('click', () => {
     }
 
    
-
+    updateCart();
     closeModal();
 });
+
+function updateCart(){
+    if(cart.length > 0){
+        c('aside').classList.add('show');
+        for(let i in cart){
+            let pizzaItem = pizzaJson.find((item) => {
+                return item.id == cart[i].id;
+            });
+
+           
+        }
+    } else {
+        c('aside').classList.remove('show');
+    }
+}
